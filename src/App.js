49c0a739f07af19803307e15114b364e3f0aa29f
Erastus Mauliate <emauliate17@gmail.com>
2021-08-03 12:28:03 +0700

    Updated inputNumber

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import "assets/scss/style.scss";
 import LandingPage from "pages/LandingPage";
+import Example from "pages/Example";
+
+import "assets/scss/style.scss";
 
 function App() {
   return <div className="App">
     <Router>
       <Route path="/" component={LandingPage}></Route>
+      <Route path="/" component={Example}></Route>
     </Router>
   </div>;
 }
